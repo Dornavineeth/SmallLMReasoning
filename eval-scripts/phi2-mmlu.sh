e284ee38d6011068d99b898c7c428e869631d312
achauhan_umass_edu <achauhan_umass_edu@gpu012.unity.rc.umass.edu>
2024-02-16 15:24:48 +0000

    Feat: Phi2 performance on subset of MMLU Algebric Dataset

---
+++ eval-scripts/phi2-mmlu.sh
@@ -0,0 +1,8 @@
+lm_eval --model hf \
+    --model_args pretrained=microsoft/phi-2,parallelize=True \
+    --tasks mmlu_flan_cot_fewshot_abstract_algebra \
+    --batch_size auto:1 \
+    --output_path results/phi-2/mmlu/ \
+    --log_samples \
+    --seed 0 \
+    --limit 10
