642f97ca16dd543047b326506a3eba2174f47c72
achauhan_umass_edu <achauhan_umass_edu@cpu020.unity.rc.umass.edu>
2024-02-19 15:08:45 +0000

    Feat: Script to generate inference files - MMLU

--- eval-scripts/filter_results.py
+++ eval-scripts/filter_results.py
@@ -1,18 +1,22 @@
 import json
+import os
 
-file = open('results/phi-2/mmlu/humanities/pretrained__microsoft__phi-2,parallelize__True_mmlu_flan_cot_fewshot_formal_logic.jsonl')
+for file_name in os.listdir('results/phi-2/mmlu/humanities/'):
+    if file_name == 'results.json':
+        continue
+    folder_path = "inference/mmlu/humanities/"
+    end_idx = file_name.index('.jsonl')
+    inference_file_name = file_name[69:end_idx] + '.txt'
+    file = open(f'results/phi-2/mmlu/humanities/{file_name}')
 
-data = json.load(file)
+    data = json.load(file)
 
-incorrect_solutions = ""
+    incorrect_solutions = ""
 
-for test_case in data:
-    if test_case['exact_match'] == 0.0:
-        incorrect_solutions+= "Question: " + test_case['doc']['question'] + "\nChoices:\n" + '\n'.join(test_case['doc']['choices']) + "\nCorrect Answer: " + test_case['target'] + "\nGenerated Reasoning: " + test_case['resps'][0][0] + "\n\n\n" 
+    for test_case in data:
+        if test_case['exact_match'] == 0.0:
+            incorrect_solutions+= "Question: " + test_case['doc']['question'] + "\nChoices:\n" + '\n'.join(test_case['doc']['choices']) + "\nCorrect Answer: " + test_case['target'] + "\nGenerated Reasoning: " + test_case['resps'][0][0] + "\n\n\n" 
 
-folder_path = "inference/mmlu/humanities/"
-file_name = 'formal_logic.txt'
-
-with open(f"{folder_path}{file_name}", 'w') as f:
-    f.write(incorrect_solutions)
-    f.close()
+    with open(f"{folder_path}{inference_file_name}", 'w') as f:
+        f.write(incorrect_solutions)
+        f.close()
