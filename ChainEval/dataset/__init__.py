96896e1f42879804a1ad2b8b9936261c0863cfd7
achauhan_umass_edu <achauhan_umass_edu@gpu003.unity.rc.umass.edu>
2024-03-11 17:54:29 +0000

    BBH benchmark integration

--- ChainEval/dataset/__init__.py
+++ ChainEval/dataset/__init__.py
@@ -1,14 +1,31 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+import requests, json
 
 def get_dataset(config):
     if config['dataset_name'] == 'gsm8k':
         # Loading the hugging face dataset
         dataset = load_dataset(**config['dataset_kwargs'])
+    elif config['dataset_name'] == 'bbh':
+        dataset = []
+        # Extract the sub-task name
+        task = config['dataset_kwargs']['name']
+        # Link to the JSON files
+        BASE_URL = "https://github.com/suzgunmirac/BIG-Bench-Hard/blob/main/bbh/"
+        # Load the JSON files 
+        loaded_dataset = json.loads(requests.get(f"{BASE_URL}{task}.json").text)
+        # Extract the question and answers from the file
+        loaded_dataset = json.loads(loaded_dataset['payload']['blob']['rawLines'][0])["examples"]
+        for item in loaded_dataset:
+            pair = {'question': item['input'], 'answer': item['target']}
+            dataset.append(pair)
+        # Create a dataset from the list of dictionaries
+        dataset = Dataset.from_list(dataset)
     else:
         raise NotImplementedError
     # return the loaded dataset (stored in cache)
     numbered_dataset = dataset.map(add_numbering, with_indices=True)
     return numbered_dataset
+    
 
 def add_numbering(example, idx):
     example_new = {}
--- ChainEval/prompts/__init__.py
+++ ChainEval/prompts/__init__.py
@@ -6,6 +6,8 @@ def get_prompts(config):
         return GSM8K_FEWSHOT
     elif config['prompt_name'] == 'GSM8K_SOT':
         return GSM8K_SKELETON, GSM8K_REASONING, GSM8K_ANSWER
+    elif config['prompt_name'] == 'BBH_FEWSHOT':
+        return BBH_FEWSHOT[config['task_name']]
     else:
         raise NotImplementedError
     
